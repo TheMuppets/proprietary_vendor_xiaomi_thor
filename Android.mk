ce1810a3075628010df7983d9011c2e255cf386c
Arian <arian.kulmer@web.de>
2025-05-24 11:35:21 +0200

    thor: Initial import from OS2.0.12.0.VLACNXM
    Matching https://github.com/LineageOS/android_device_xiaomi_thor/tree/e3d113111d8e2c01e8e360712f53e25b17718842

---
+++ Android.mk
@@ -0,0 +1,31 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),thor)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,705bda01d3e3c56349eeb8337d09d8d13225b365)
+$(call add-radio-file-sha1-checked,radio/aop.img,22c56955dcb6f30a5e0c53c95690ba0fc34f9a39)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,211fb17461fe931073a7efd304694ea71fdf7624)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,b9bce86675077709c1ffd39484c64b4134e46214)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,00c7b51c94d0945f09490ecc8278a8bfd4fe98ca)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,40c4ad590e69ad6dea6daac71067ba43edc29a87)
+$(call add-radio-file-sha1-checked,radio/dsp.img,259329c9822fa19d5ba7f6e06794c44ceec3d220)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,6b4eb0b1fea34c591e4bf2ecefc10ed8ce58a57b)
+$(call add-radio-file-sha1-checked,radio/hyp.img,c6c778bcbfe95195c82343360cce2934da542d62)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,521bb920fdb1d9a6c2f35b020a2e5b006293784a)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,57187daa39d5369d8c7a5cc21ed1ca1b4d7ab0ba)
+$(call add-radio-file-sha1-checked,radio/modem.img,0bf8a22a94c8dc794afc6966ca38eb998901802a)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,cd9a74acca84b549056ca06875cffdefbc4f0aa3)
+$(call add-radio-file-sha1-checked,radio/shrm.img,73b8d2e26086731c315d14f967a4936039826d61)
+$(call add-radio-file-sha1-checked,radio/tz.img,a9ea5cdc25ddf8523c7c80074383277b3847efab)
+$(call add-radio-file-sha1-checked,radio/uefi.img,5de60780f5ac2d5aeca0cae96fe8ddf6c733968f)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,4aa19d966a596a1ec991c64262b2f0cf4e00697c)
+$(call add-radio-file-sha1-checked,radio/vm-bootsys.img,ba20165ced28dca887728c18dedd4135ad492976)
+$(call add-radio-file-sha1-checked,radio/xbl.img,de3cffa8de24f5dcc56ca5ede833b8346ca27b0b)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,e2c0d4b05496fae4fbdfa75b633490bdf9bac7a9)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,c1fa1f27b387cf434d1ec9a928862ff3b7f669b4)
+
+endif
